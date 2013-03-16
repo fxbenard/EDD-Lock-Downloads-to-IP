943716288ced8c4820da0e72db58a1046387f99b
Fx Benard <fxbenard@gmail.com>
2013-03-16 16:26:47 +0100

    [i18n]> language files : pot, french and strings

---
+++ languages/strings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file holds localisation strings that are scanned by localisation plugins. It is not included anywhere.
+ *
+ * @package     Easy Digital Downloads - Lock Downloads to IP
+ * @subpackage  Strings
+ * @copyright   Copyright (c) 2013, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+__( 'Easy Digital Downloads - Lock Downloads to IP', 'edd-iplock' );
+__( 'An extension for Easy Digital Downloads to lock file downloads to the IP address used to purchase the file', 'edd-iplock' );
+__( 'Pippin Williamson', 'edd-iplock' );
